4794e5ad15c797376b001dfe8788d301c44239aa
karencfisher <karen-fisher@lambdastudents.com>
2020-08-07 02:41:13 -0600

    added twitter and basilica, update DB

--- web_app/db/db_model.py
+++ web_app/db/db_model.py
@@ -7,18 +7,20 @@ migrate = Migrate()
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(128))
-    followers = db.Column(db.Integer)
+    id = db.Column(db.BigInteger, primary_key=True)
+    user = db.Column(db.String(128), nullable=False)
+    newest_tweet_id = db.Column(db.BigInteger, nullable=False)
 
     def __repr__(self):
         return f'<User {self.id} {self.name}>'
 
 
 class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tweet = db.Column(db.String(300))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.BigInteger, primary_key=True)
+    tweet = db.Column(db.String(300), nullable=False)
+    embedding = db.Column(db.PickleType, nullable=False)
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('tweet', lazy=True))
 
     def __repr__(self):
         return f'<Tweet {self.tweet}>'
