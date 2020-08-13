48dcb3c99288412e363c8e1c47a01f8f671a49db
karencfisher <karen-fisher@lambdastudents.com>
2020-08-12 22:37:35 -0600

    try hard coded DB URI

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -8,7 +8,9 @@ from web_app.routes.twitoff_routes import twitoff_routes
 
 load_dotenv()
 DATABASE_URI = 'sqlite:///db\\twitoff_db.sqlite'
-DB_URL = getenv('DB_URL')
+#DB_URL = getenv('DB_URL')
+DB_URL = 'postgres://lrelfakuvrkuui:e34cfbe7bc3ce4cf7fca0603346129f947755a11cd0bb638c7c08c8303d21101@ec2-184-73-249-9.compute-1.amazonaws.com:5432/dbj1kjrvlimh7n'
+
 
 def create_app():
     print('Initializing...')
